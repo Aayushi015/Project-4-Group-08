21a5b242814287425119c0dddb9c1d8d1f3ced3d
NguyenThiKimKhue”git config --global user.password “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 <kimkhuestar@gmail.com>
2025-04-06 19:56:28 +0700

    Updates

--- app_level3/modelHelper.py
+++ app_level3/modelHelper.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 import pickle
 
+# //..The model Helper class we built and tested it in Jupyter Notebook with a function makePrediction that bring in all unique values to a data frame
+# // .. opens up the model, makes the inference, and return the inference back to the user.
+
 class ModelHelper():
     def __init__(self):
         pass
-
+    # // Our function makePrediction will bring in all unique values to a data frame
     def make_predictions(self, amount_usd, transaction_type, industry, reported_by_authority, risk_score, shell_co_involved):
         df = pd.DataFrame()
         df["amount_usd"] = [amount_usd]  
@@ -15,7 +18,7 @@ class ModelHelper():
         df["risk_score"] = [risk_score]
         df["shell_co_involved"] = [shell_co_involved]
 
-        # Load model
+        # Load model (opens up the model, makes the inference, and return the inference back to the user)
         model = pickle.load(open("black_money_model.h5", 'rb'))
 
         preds = model.predict_proba(df)
--- app_level3/static/js/logic.js
+++ app_level3/static/js/logic.js
@@ -8,17 +8,24 @@ $(document).ready(function() {
 });
 
 
-    // call Flask API endpoint
+    // call Flask API endpoint (Use JavaScripts to reate makePredictions function.)
+    // Note: This grab the values from html for all options in our form, make those variables into a payload dictionary below. 
+    // Then call an Ajax POST request to send that payload to backend (app.py)
+    // App.py receives the payload, then extract and parse and fix the data types of values to payload
 function makePredictions() {
     let amount_usd = $("#amount_usd").val();
-    let transaction_type = $("transaction_type").val();
-    let industry = $("industry").val();
-    let reported_by_authority = $("reported_by_authority").val();
-    let risk_score = $("risk_score").val();
-    let shell_co_involved = $("shell_co_involved").val();
+    let transaction_type = $("#transaction_type").val();
+    let industry = $("#industry").val();
+    let reported_by_authority = $("#reported_by_authority").val();
+    let risk_score = $("#risk_score").val();
+    let shell_co_involved = $("#shell_co_involved").val();
 
+    // check if inputs are valid (can add more validation here)
+    if (!amount_usd || !transaction_type || !industry || !reported_by_authority || !risk_score || !shell_co_involved) {
+        alert("Please fill in all fields before submitting!");
+        return;
+    }
 
-    // check if inputs are valid
 
     // create the payload
     let payload = {
@@ -39,19 +46,60 @@ function makePredictions() {
         success: function(returnedData) {
             // print it
             console.log(returnedData);
+            // JS receives the response from modelHelper/ We extract and parse the response to show the user the below outcome.
             let prob = parseFloat(returnedData["prediction"]);
 
             if (prob > 0.5) {
                 $("#output").text(`Source of money is legal with probability ${prob}!`);
             } else {
-                $("#output").text(`YSource of money is illegal with probability ${prob}. :(`);
+                $("#output").text(`Source of money is illegal with probability ${prob}. :(`);
             }
 
+            // Call buildDonut function
+            buildDonut(prob)
         },
+
+
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            alert("Status: " + textStatus);
-            alert("Error: " + errorThrown);
+            console.error("Error: ", errorThrown);
+            console.error("Status: ", textStatus);
+            console.error("Response Text: ", XMLHttpRequest.responseText);
+            alert("Error occurred. Please check the console for details.");
         }
+
     });
 
 }
+
+function buildDonut(prob) {
+    // Data
+    let legal = prob
+    let illegal = 1-prob;
+
+    let data = [{
+        values: [legal, illegal],
+        labels: ['Legal', 'illegal']
+        hole: .5,
+        marker: {
+            colors: ['blue', 'red']
+        },
+        textinfo:"label+percent",
+        type: 'pie'
+    }],
+
+    let layout = {
+        annotation: [{
+            font: {size: 30},
+            showarrow: false
+            text: 'Legal',
+            x: 0.5,
+            y: 0.5
+        }],
+        height: 800,
+        width: 1200,
+        showlegend: false
+    };
+
+    Plotly.newPlot('donut', data,layout);
+
+};
